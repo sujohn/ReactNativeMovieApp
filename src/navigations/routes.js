5442a37ba86c5a88477146e7d2356ba02bc4a792
Shaiful Islam Sujohn <sujohn_cse@yahoo.com>
2020-03-15 00:49:17 +0600

    Route file added.

---
+++ src/navigations/routes.js
@@ -0,0 +1,14 @@
+export const ROUTES = {
+    MOVIE_LIST: 'MovieList',
+    MOVIE_DETAILS: 'MovieDetails',
+    MOVIE_VIDEO: 'MovieVideo',
+    SEARCH: 'Search',
+    SEARCH_RESULTS: 'SearchResults',
+    CONFIGURATION: 'Configuration'
+};
+
+export const TABS = {
+    HOME: 'Home',
+    SEARCH: 'Search',
+    CONFIG: 'More'
+};
